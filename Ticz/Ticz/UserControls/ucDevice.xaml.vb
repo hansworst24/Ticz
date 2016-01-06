412cba500287b14bce7330485e2fb8d60f40b937
hansworst24 <tvhead@outlook.com>
2016-01-06 16:45:08 +0100

    rename usercontrol myLightSwitch to ucDevice to better reflect what it represents

--- Ticz/Ticz/UserControls/ucDevice.xaml.vb
+++ Ticz/Ticz/UserControls/ucDevice.xaml.vb
@@ -1,5 +1,5 @@
 ﻿' The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
-Public NotInheritable Class myLightSwitch
+Public NotInheritable Class ucDevice
     Inherits UserControl
 End Class
