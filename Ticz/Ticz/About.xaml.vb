89cead1d8aa7c12239d77779c0d4b54912b0461c
hansworst24 <tvhead@outlook.com>
2016-01-12 14:23:30 +0100

    hide commandbar, show BackButton when backstack isnt empty

--- Ticz/Ticz/About.xaml.vb
+++ Ticz/Ticz/About.xaml.vb
@@ -1,5 +1,6 @@
 ﻿' The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
+Imports Windows.UI.Core
 ''' <summary>
 ''' An empty page that can be used on its own or navigated to within a Frame.
 ''' </summary>
@@ -11,6 +12,13 @@ Public NotInheritable Class AboutPage
 
     Protected Overrides Sub OnNavigatedTo(e As NavigationEventArgs)
         Me.DataContext = app.myViewModel
+        Dim rootFrame As Frame = CType(Window.Current.Content, Frame)
+        If rootFrame.CanGoBack Then
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible
+        Else
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed
+        End If
+
     End Sub
 
     Protected Overrides Sub OnNavigatedFrom(e As NavigationEventArgs)
