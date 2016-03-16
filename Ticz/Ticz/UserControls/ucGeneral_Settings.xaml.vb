7450b14ffdcbcba410ed12877d33561e715dd062
hansworst24 <tvhead@outlook.com>
2016-03-16 11:19:24 +0100

    add some missing files from project to git

---
+++ Ticz/Ticz/UserControls/ucGeneral_Settings.xaml.vb
@@ -0,0 +1,6 @@
+﻿' The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+Public NotInheritable Class ucGeneral_Settings
+    Inherits UserControl
+
+End Class
